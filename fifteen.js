edabbacc97065d51a6c76c4bc0539f7bceb3941b
Tori-Anne <connell_tori@yahoo.com>
2018-11-06 10:43:17 -0500

    background image

--- fifteen.js
+++ fifteen.js
@@ -1,1 +1,11 @@
-//Javascript
+
+// set image as background-image of each puzzle piece
+function setBackgroundImage(){
+    tiles = document.getElementById("puzzlearea").innerHTML;
+    for (var i = 0; i < tiles.length - 1; i++){
+        tiles[i].addClassName("puzzlepiece");
+        tiles[i].style.backgroundImage = "url('background.jpg')";
+		//background position
+    }
+}
+	
