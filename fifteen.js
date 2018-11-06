3bafec9ec791950a9c1e82b92ed90064b5140bdf
Tori-Anne <connell_tori@yahoo.com>
2018-11-06 16:50:39 -0500

    tiles

--- fifteen.js
+++ fifteen.js
@@ -1,11 +1,11 @@
 
 // set image as background-image of each puzzle piece
-function setBackgroundImage(){
-    tiles = document.getElementById("puzzlearea").innerHTML;
-    for (var i = 0; i < tiles.length - 1; i++){
-        tiles[i].addClassName("puzzlepiece");
-        tiles[i].style.backgroundImage = "url('background.jpg')";
-		//background position
+function setPuzzlePieces(){
+    var tiles = document.getElementById("puzzlearea");
+    var puzzlePiece = tiles.getElementsByTagName("div");
+    for (var i = 0; i < puzzlePiece.length - 1; i++){
+        puzzlePiece[i].addClassName("puzzlepiece");
+        puzzlePiece[i].style.backgroundImage = "url('background.jpg')";
+        puzzlePiece[i].style.backgroundPosition = "-304px 0px";
     }
 }
-	
