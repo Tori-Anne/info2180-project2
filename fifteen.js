508beda10e3cafcc6a68e0d39c4ef507b6b3009b
Tori-Anne <connell_tori@yahoo.com>
2018-11-06 22:43:16 -0500

    shuffleTiles

--- fifteen.js
+++ fifteen.js
@@ -17,10 +17,20 @@ function isMovable(){
 	var tiles = document.getElementById("puzzlearea");
     var puzzlePiece = tiles.getElementsByTagName("div");
     for (var i = 0; i < puzzlePiece.length - 1; i++){
-		if (puzzlePiece[i].innerHTML != " "){
-			puzzlePiece[i].addEventListener("onmouseover", function() {
+		if (puzzlePiece[i].innerHTML != ""){
+			puzzlePiece[i].addEventListener("mouseover", function(){
                 puzzlePiece[i].addClassName("movablepiece");
 		    }
 		}
 	}
+}
+
+
+
+// when the shuffle button is clicked, the tiles of the puzzle are randomized
+function shuffleTiles(){
+	var shuffleButton = document.getElementById("shufflebutton");
+	shuffleButton.addEventListener("click", function(){
+		
+	
 }
